dbf74a0cfa5cdb928f284da30a8b26688f281dc3
Kadir Altay <kadiraltay90@gmail.com>
2026-06-19 18:22:50 +0300

    configured whitenoise for production admin static files

--- metal_art_shop/wsgi.py
+++ metal_art_shop/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for metal_art_shop project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise  # KANKA: Whitenoise'u buraya çağırıyoruz
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metal_art_shop.settings')
 
 application = get_wsgi_application()
+
+# KANKA: Django ayağa kalkarken statik dosyaları bu satır sayesinde dünyaya aslanlar gibi servis edecek!
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles'))
